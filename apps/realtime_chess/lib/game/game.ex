f8c3c7f60210804ceff2c707436841a51b861e82
Satish Kunisi <kunisi@gmail.com>
2018-10-27 10:19:11 -0700

    Remove unused functions from Game

--- apps/realtime_chess/lib/game/game.ex
+++ apps/realtime_chess/lib/game/game.ex
@@ -1,7 +1,4 @@
 defmodule RealtimeChess.Game do
-  alias __MODULE__
-  alias RealtimeChess.Game.GameState
-
   defstruct name: "", board: %{}, status: :unstarted #, player_white: %Player{}, player_black: %Player{}, board: %Board{}
   @type t :: %__MODULE__{name: String.t, board: map, status: atom}
 
